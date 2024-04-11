45843f902b9c86390730f053540b9ab85d559f0c
Xinyu Shen <shenxinyu@ufl.edu>
2024-04-11 15:51:27 -0400

    test

--- test_Lab9.py
+++ test_Lab9.py
@@ -7,8 +7,11 @@ def encoder(password):
 
 
 def decoder(password):
-    pass
-    print("hello")
+    decoded_password = ""
+    for digit in password:
+        decoded_digit = (int(digit) - 3) % 10
+        decoded_password += str(decoded_digit)
+    return decoded_password
 
 def main():
     pass
